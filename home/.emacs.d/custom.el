d65e5533321a89c4ce0b9da629d65026491edf84
Mike Barker <mike@thebarkers.com>
2021-06-20 22:53:59 -0600

    Ignore emacs-config.el file

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -3,7 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t)
  '(package-selected-packages '(evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
