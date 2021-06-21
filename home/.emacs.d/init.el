cc4c74436a3df2efe564fa6d065ddcd662079f69
Mike Barker <mike@thebarkers.com>
2021-06-20 21:56:37 -0600

    Move all config to emacs-config.org

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -2,40 +2,8 @@
 ;; Mike Barker
 ;; February 12th, 2021
 
-;; Add MELPA package server
-(require 'package)
+;; My emacs configuration file
+(defvar user/emacs-config-file (concat user-emacs-directory "emacs-config.org"))
 
-(add-to-list 'package-archives
-  '("melpa" . "https://melpa.org/packages/") t)
-
-(unless package-archive-contents
-  (package-refresh-contents))
-
-(package-initialize)
-
-
-;; Install package use-package
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-
-
-;; Load org package
-(require 'org)
-
-
-;; Load our emacs config
-(org-babel-load-file "~/.emacs.d/emacs-config.org")
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t)
- '(package-selected-packages '(evil use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; Load my emacs config
+(org-babel-load-file user/emacs-config-file)
