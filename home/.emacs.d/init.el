7a0fbb6a47e1c277dbad473810e9b63e26881c91
Mike Barker <mike@thebarkers.com>
2023-12-24 08:24:56 -0700

    Configuring Emacs video series
    I watched Deric Taylor's Configuring Emacs video series
    and changed my emacs org mode config based on his settings.
    https://gitlab.com/dwt1/configuring-emacs
    
    Some changes:
    - I am using emacs' default package manager and use-package
    - I am not using any of the 'doom' themes or modeline support
    - I added 'os' specisific configuration and this config handles
      light/dark mode switching on macOS systems.

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -1,9 +1,4 @@
-;; init.el --- My emacs initialization
-;; Mike Barker
-;; February 12th, 2021
-
-;; My emacs configuration file
-(defvar user/emacs-config-file (concat user-emacs-directory "emacs-config.org"))
-
-;; Load my emacs config
-(org-babel-load-file user/emacs-config-file)
+(org-babel-load-file
+ (expand-file-name
+ "config.org"
+  user-emacs-directory))
